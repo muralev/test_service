1e0cf9944e5ab59c389b4a6866a27374eff03b55
muralev <muralev.aleksei@gmail.com>
2020-08-26 01:34:34 +0500

    Change app file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import pandas as pd
 import pickle
-
+import fasttext
 
 
 from flask import Flask
@@ -21,7 +21,8 @@ application = Flask(__name__)
 
 #загружаем модели из файла
 vec = pickle.load(open("./models/tfidf.pickle", "rb"))
-model = lgb.Booster(model_file='./models/lgbm_model.txt')
+#model = lgb.Booster(model_file='./models/lgbm_model.txt')
+model = fasttext.load_model("./models/fastText_model.txt")
 
 
 # тестовый вывод
@@ -34,6 +35,7 @@ def hello():
     return response
 
 # предикт категории
+#{"user_message":"example123rfssg gsfgfd"}
 @application.route("/categoryPrediction" , methods=['GET', 'POST'])  
 def registration():
     resp = {'message':'ok'
@@ -45,8 +47,14 @@ def registration():
         json_params = json.loads(getData) 
         
         #напишите прогноз и верните его в ответе в параметре 'prediction'
-
-
+        usr_msg = json_params['user_message']
+        if (usr_msg is None) or (usr_msg == ''):
+            resp['message'] = str('error!')
+            resp['category'] = str('Empty message.')
+        else:    
+            category = model.predict(usr_msg,k=3)[1]
+            category = category.tolist()
+            resp['category'] = category
 
         
     except Exception as e: 
@@ -62,6 +70,3 @@ def registration():
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
     application.run(debug=False, port=port, host='0.0.0.0' , threaded=True)
-
-
-
